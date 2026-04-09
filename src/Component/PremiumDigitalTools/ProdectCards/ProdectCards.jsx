1c5f67e19885ac1c88be5fdd5104091d8870b53c
shantodey <ratul3330@gmail.com>
2026-04-10 00:10:16 +0600

    fixed responsive ness

--- src/Component/PremiumDigitalTools/ProdectCards/ProdectCards.jsx
+++ src/Component/PremiumDigitalTools/ProdectCards/ProdectCards.jsx
@@ -4,7 +4,7 @@ import Card from '../ProdectCards/Card/Card'
 const Cards = ({ getProductsData,cats,setCats  }) => {
     const receveData=use(getProductsData)
     return (
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
             {receveData.map((prodectsData) => (
                 <Card 
                     key={prodectsData.id}
